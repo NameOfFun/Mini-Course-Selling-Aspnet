0593b5ec3c73c2f4429b50f4f88e8167777f1ce7
NameOfFun <12damhao@gmail.com>
2026-04-12 17:53:44 +0700

    login register

---
+++ Dtos/AuthResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Course_Selling_System.Dtos
+{
+    public class AuthResponse
+    {
+        public string Token { get; set; } = null!;
+        public DateTime ExpiresAtUtc { get; set; }
+        public int UserId { get; set; }
+        public string Email { get; set; } = null!;
+        public string Role { get; set; } = null!;
+    }
+}
---
+++ Dtos/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course_Selling_System.Dtos
+{
+    public class RegisterRequest
+    {
+        [Required, MaxLength(255)]
+        public string FullName { get; set; } = null!;
+        [Required, EmailAddress, MaxLength(255)]
+        public string Email { get; set; } = null!;
+        [Required, MaxLength(255)]
+        public string Password { get; set; } = null!;
+    }
+}
