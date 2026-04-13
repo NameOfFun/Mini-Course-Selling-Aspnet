cfd54ced6417ba24c6b57aa13ece34b626b9efae
NameOfFun <12damhao@gmail.com>
2026-04-13 22:59:49 +0700

    custommer request

---
+++ Dtos/CreateOrderRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course_Selling_System.Dtos
+{
+    public class CreateOrderRequest
+    {
+        [Required, MinLength(1)]
+        public IList<int> CourseIds { get; set; } = new List<int>();
+    }
+}
